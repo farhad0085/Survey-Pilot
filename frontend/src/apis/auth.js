8c532ebda6aee90a8ab190f2abdec2385e82a82c
Farhad Hossain <farhadhossain0085@gmail.com>
2024-08-25 20:05:22 +0600

    deleted old frontend

--- frontend/src/apis/auth.js
+++
@@ -1,6 +0,0 @@
-import axios, { getHeaders } from '../utils/axios';
-
-export const registerUser = (userData) => axios.post(`/register`, userData);
-export const loginUser = (credentials) => axios.post(`/api/auth/login/`, credentials);
-export const userInfo = () => axios.get("/api/auth/user/me", {headers: getHeaders()});
-export const getDashboardData = () => axios.get("/api/auth/dashboard/", {headers: getHeaders()});
